0e1eabdaca4c17680aa0e8f8b496949877264e5a
debmalyajash <debmalya.jash@gmail.com>
2017-04-07 22:11:58 +0800

    Tagging

--- src/main/java/org/deb/TagRawDescription.java
+++ src/main/java/org/deb/TagRawDescription.java
@@ -15,13 +15,28 @@
  */
 package org.deb;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import com.opencsv.CSVReader;
+import org.apache.log4j.Logger;
+
+import com.google.cloud.language.spi.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
+import com.google.cloud.language.v1.AnalyzeSyntaxResponse;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.EncodingType;
+import com.google.cloud.language.v1.PartOfSpeech.Tag;
+import com.google.cloud.language.v1.Token;
 
 /**
  * @author debmalyajash
@@ -29,58 +44,209 @@ import com.opencsv.CSVReader;
  */
 public class TagRawDescription {
 
+	static int trainingLineCount = 0;
+
+	static int testLineCount = 0;
+
+	static int withoutTag = 0;
+
+	static Set<String> criteriaSet = new HashSet<>();
+
+	static List<Integer> lineNoWithoutTag = new ArrayList<>();
+
+	private static final Logger LOGGER = Logger.getLogger(TagRawDescription.class);
+
+	String[] removalList = new String[] { "  ", "the", "company" };
+
+	String[] keyIngredients = new String[] { "years of experience", "associate", "supervise", "ms or phd", "ms-or-phd",
+			"supervising", "hourly", "salary", "part time", "part-time", "licence", "full time", "full-time",
+			"bs-degree", "bs degree", "college degree" };
+
+	String[] description = new String[] { "years-experience-needed", "associate-needed", "supervising-job",
+			"ms-or-phd-needed", "ms-or-phd-needed", "supervising-job", "hourly-wage", "salary", "part-time-job",
+			"part-time-job", "licence-needed", "full-time-job", "full-time-job", "bs-degree-needed", "bs-degree-needed",
+			"bs-degree-needed" };
+
+	// Key is tag, value is job description
+	Map<String, String> tagJobDescriptionMap = new HashMap<>();
+
+	// Instantiates a client
+	LanguageServiceClient language;
+
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		if (args.length > 1){
-			train(args[0]);
-			tag(args[1]);
+		if (args.length > 1) {
+			TagRawDescription tag = new TagRawDescription();
+			tag.train(args[0]);
+			tag.tag(args[1]);
 		} else {
 			System.err.println("Usage : java org.deb.TagRawDescription <trainging file> <test file>");
 		}
-		
 
+	}
+
+	public TagRawDescription() throws IOException {
+		language = LanguageServiceClient.create();
 	}
 
 	/**
 	 * @param testFileName
-	 * @throws IOException 
-	 * @throws FileNotFoundException 
+	 * @throws IOException
+	 * @throws FileNotFoundException
 	 */
-	public static void tag(String testFileName) throws IOException {
-		try (CSVReader reader = new CSVReader(new FileReader(testFileName), '\t')){
-			List<String[]> allLines = new ArrayList<>();
-			String[] eachLine = reader.readNext();
-			while (eachLine != null){
-				
-				System.out.println("Line :" + eachLine[0]);
-				System.out.println("Tag :" + eachLine[1]);
-				allLines.add(eachLine);
-				eachLine = reader.readNext();
+	public void tag(String testFileName) throws IOException {
+		PrintWriter writer = null;
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(testFileName))) {
+			writer = new PrintWriter("tags.tsv");
+			writer.println("tags");
+			String eachLine = bufferedReader.readLine();
+			boolean firstLine = true;
+			StringBuilder tag = new StringBuilder();
+			while (eachLine != null) {
+				testLineCount++;
+
+				eachLine = eachLine.toLowerCase();
+				if (!firstLine) {
+					if (eachLine == null || eachLine.trim().length() == 0) {
+
+					} else {
+						String jobDescription = tagJobDescriptionMap.get(eachLine);
+						if (jobDescription != null) {
+							tag.append(jobDescription);
+						} else {
+							for (int i = 0; i < keyIngredients.length; i++) {
+								if (eachLine.contains(keyIngredients[i])) {
+									if (tag.length() > 0) {
+										tag.append(" ");
+									}
+									if (i == 0) {
+										// find years of experience required
+										int index = eachLine.indexOf(keyIngredients[i]);
+										String[] yearFinder = eachLine.substring(0, index).split(" ");
+										if (yearFinder[yearFinder.length - 1].equals("1")) {
+											tag.append("1-year-experience-needed");
+										} else if (yearFinder[yearFinder.length - 1].contains("2")
+												|| yearFinder[yearFinder.length - 1].contains("3")
+												|| yearFinder[yearFinder.length - 1].contains("4")
+												|| yearFinder[yearFinder.length - 1].contains("2-4")) {
+											tag.append("2-4-years-experience-needed");
+										} else {
+											tag.append("5-plus-years-experience-needed");
+										}
+
+									} else {
+										tag.append(description[i]);
+									}
+
+								}
+							}
+						}
+
+						if (tag.length() == 0) {
+							// if not able to tag to any job description
+						}
+					}
+					writer.println(tag.toString());
+					tag.delete(0, tag.length());
+				} else {
+					firstLine = false;
+				}
+				eachLine = bufferedReader.readLine();
 			}
-		
-			System.out.println("Total number of lines to tag:" + allLines.size());
+		} catch (Throwable th) {
+
 		} finally {
-			
+			if (writer != null) {
+				writer.flush();
+				writer.close();
+			}
 		}
-		
+
 	}
 
 	/**
 	 * @param trainingFileName
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static void train(String trainingFileName) throws IOException {
-		try (CSVReader reader = new CSVReader(new FileReader(trainingFileName), '\t')){
-			List<String[]> allLines = reader.readAll();
-			System.out.println("Total number of lines to train:" + allLines.size());
+	public void train(String trainingFileName) throws IOException {
+
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(trainingFileName))) {
+
+			String eachLine = bufferedReader.readLine();
+			boolean firstLine = true;
+			while (eachLine != null) {
+				if (!firstLine) {
+					String fields[] = eachLine.split(",");
+
+					if (fields[0].trim().length() == 0) {
+						lineNoWithoutTag.add(trainingLineCount);
+						withoutTag++;
+						// LOGGER.error(trainingLineCount + ") " + eachLine);
+					} else {
+						String[] mainCrieteria = fields[0].split(" ");
+						tagJobDescriptionMap.put(fields[1], fields[0]);
+						for (String eachCriteria : mainCrieteria) {
+							criteriaSet.add(eachCriteria);
+						}
+					}
+				} else {
+					firstLine = false;
+				}
+				trainingLineCount++;
+				eachLine = bufferedReader.readLine();
+			}
+		} catch (Throwable th) {
+
 		} finally {
-			
+			if (withoutTag > 0) {
+				LOGGER.error("Without tag count :" + withoutTag);
+				// LOGGER.error("Line nos. :" + lineNoWithoutTag);
+			}
+			LOGGER.debug(criteriaSet);
 		}
-		
-		
+
+	}
+
+	/**
+	 * 
+	 * @param jobDescription
+	 * @return
+	 */
+	public List<String> getKeyWords(final String jobDescription) {
+		List<String> keyWordList = new ArrayList<>();
+		Document doc = Document.newBuilder().setContent(jobDescription).setType(Type.PLAIN_TEXT).build();
+		EncodingType encodingType = EncodingType.NONE;
+
+		AnalyzeEntitiesResponse entities = language.analyzeEntities(doc, encodingType);
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug(entities);
+		}
+		AnalyzeSyntaxResponse response = language.analyzeSyntax(doc, encodingType);
+
+		List<Token> tokenList = response.getTokensList();
+		for (Token eachToken : tokenList) {
+			Tag eachTag = eachToken.getPartOfSpeech().getTag();
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug(eachToken.toString());
+			}
+			if (eachTag == Tag.NOUN || eachTag == Tag.ADJ || eachTag == Tag.VERB) {
+				keyWordList.add(eachToken.getLemma());
+			}
+		}
+
+		for (String eachIngredient : keyIngredients) {
+			if (jobDescription.contains(eachIngredient)) {
+				keyWordList.add(eachIngredient);
+			}
+		}
+
+		return keyWordList;
 	}
 
 }
+
+// Reference :
+// https://cloud.google.com/natural-language/docs/reference/libraries#installing_the_client_library
--- src/test/java/org/deb/TagRawDescriptionTest.java
+++ src/test/java/org/deb/TagRawDescriptionTest.java
@@ -15,8 +15,8 @@
  */
 package org.deb;
 
-import static org.junit.Assert.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -29,17 +29,74 @@ import org.junit.Test;
 public class TagRawDescriptionTest {
 
 	/**
-	 * Test method for {@link org.deb.TagRawDescription#main(java.lang.String[])}.
+	 * Test method for
+	 * {@link org.deb.TagRawDescription#main(java.lang.String[])}.
 	 */
 	@Test
 	public final void testMain() {
 		try {
-			TagRawDescription.main(new String[]{"./src/main/resources/train.tsv","./src/main/resources/test.tsv"});
-//			TagRawDescription.main(new String[]{"./src/test/resources/train.tsv","./src/test/resources/test.tsv"});
+			TagRawDescription.main(new String[] { "./src/main/resources/train.csv", "./src/main/resources/test.csv" });
+			Assert.assertEquals(4376, TagRawDescription.trainingLineCount);
+			Assert.assertEquals(2922, TagRawDescription.testLineCount);
+
+			boolean firstLine = true;
+			try (BufferedReader bufferedReader = new BufferedReader(new FileReader("tags.tsv"))) {
+				String eachLine = bufferedReader.readLine();
+				int lineCount = 0;
+				while (eachLine != null) {
+					if (!firstLine) {
+						String[] tags = eachLine.split(" ");
+						for (String eachTag : tags) {
+							if (eachTag.trim().length() > 0 && !TagRawDescription.criteriaSet.contains(eachTag)) {
+								Assert.assertFalse(lineCount + ") " + eachTag + " unknown tag", true);
+							}
+						}
+					} else {
+						firstLine = false;
+					}
+					lineCount++;
+					eachLine = bufferedReader.readLine();
+				}
+
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			Assert.assertFalse(e.getMessage(),true);
+			Assert.assertFalse(e.getMessage(), true);
+		}
+	}
+
+	@Test
+	public final void testGetKeyWord() {
+		try {
+			TagRawDescription trd = new TagRawDescription();
+			trd.getKeyWords(
+					"THE COMPANY    Employer is a midstream service provider to the onshore Oil and Gas markets.  "
+							+ "It is a a fast growing filtration technology company providing environmentally sound solutions "
+							+ "to the E&P’s for water and drilling fluids management and recycling. "
+							+ "   THE POSITION    The North Dakota Regional Technical Sales Representative reports directly "
+							+ "to the VP of Sales and covers a territory that includes North Dakota and surrounding areas of South Dakota,"
+							+ " Wyoming and Montana.  Specific duties for this position include but are not limited to: "
+							+ "Building sales volume within the established territory from existing and new accounts Set up and maintain "
+							+ "a strategic sales plan for the territory Present technical presentations, "
+							+ "product demonstrations & training Maintain direct contact with customers, distributors and representatives "
+							+ "Prospect new customer contacts and referrals  "
+							+ "Gather and record customer & competitor information   Provide accurate and updated forecasts for the territory"
+							+ "   Identify new product opportunities  "
+							+ " Build long-term relationships with customers, reps & distributors    CANDIDATE REQUIREMENT    "
+							+ "The ideal candidate will possess technical degree, preferably in the oil & gas discipline and/or 5+ years of experience preferably"
+							+ " with exploration and production companies (midstream service companies are a big plus).      "
+							+ "Other desired requirements include but are not limited to:   "
+							+ "  Consistent record of superior sales results & experience closing sales   Proven ability to cold-call, develop relationships "
+							+ "  Excellent written and verbal communication skills.    Strong computer skills, including Word, Excel, PowerPoint, e-mail, etc.  "
+							+ " Strong work ethic and ability to work independently. "
+							+ "  Must be willing to develop new business – not just maintain current accounts  "
+							+ " Ability to travel extensively throughout assigned region   "
+							+ " If you are a self-motivated individual with strong engineering, and leadership skills and a desire to build a stronger,"
+							+ " more advanced organization we encourage you to apply.      "
+							+ "Position is located in North Dakota, but sales representative could live as far away as Casper, Wyoming or Billings, Montana. "
+							+ "    Successful candidates must pass a post offer background and drug screen.    EOE ");
+		} catch (Throwable th) {
+			Assert.assertFalse(th.getMessage(), true);
 		}
 	}
 
